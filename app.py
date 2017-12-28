578598eceede8524f73210d1db8f2cd9451ccbeb
M.Ike <mike.app.info@gmail.com>
2017-12-29 03:56:07 +0900

    ページ仮組み

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+jinja_options = app.jinja_options.copy()
+jinja_options.update(dict(
+    block_start_string='<%',
+    block_end_string='%>',
+    variable_start_string='%%',
+    variable_end_string='%%',
+    comment_start_string='<#',
+    comment_end_string='#>',
+))
+app.jinja_options = jinja_options
+
 if os.getenv('VCAP_APP_PORT'):
     DEBUG = False
     host = '0.0.0.0'
@@ -15,7 +26,7 @@ else:
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
